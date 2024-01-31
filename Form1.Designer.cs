1014438d536003f31b0d91442f59a0474b496bd3
rafael.gioffi <rafael.gioffi@atos.net>
2024-01-31 19:25:15 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CheckDNs___Conta_Corrente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colDN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colQuant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.opFile = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.AutoSize = true;
+            this.btnCheck.Location = new System.Drawing.Point(3, 12);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(205, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Selecione o arquivo";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colDN,
+            this.colQuant});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(205, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // colDN
+            // 
+            this.colDN.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colDN.HeaderText = "DN";
+            this.colDN.Name = "colDN";
+            this.colDN.ReadOnly = true;
+            this.colDN.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.colDN.Width = 60;
+            // 
+            // colQuant
+            // 
+            this.colQuant.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colQuant.HeaderText = "Quantidade";
+            this.colQuant.Name = "colQuant";
+            this.colQuant.ReadOnly = true;
+            // 
+            // opFile
+            // 
+            this.opFile.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colQuant;
+        private System.Windows.Forms.OpenFileDialog opFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckDNs___Conta_Corrente
+{
+    public partial class Form1 : Form
+    {
+        string fileName;
+        string[] allFile = { };
+        string[] actualDn;
+        string verifingDN;
+        int actualQuant = 0;
+        List<string> DNs = new List<string>();
+        List<int> Quants = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            opFile.ShowDialog();
+            fileName = opFile.FileName;
+            
+            try
+            {
+                if(File.Exists(fileName))
+                {
+                    allFile = File.ReadAllLines(fileName);
+                    
+                    for (int i = 0;i < allFile.Length; i++)
+                    {
+                        actualDn = allFile[i].Split('#');
+                        if (i == 0)
+                        {
+                            verifingDN = actualDn[0];
+                        }
+
+                        if (actualDn[0] == verifingDN)
+                        {
+                            actualQuant++;
+                        }
+                        else
+                        {
+                            DNs.Add(actualDn[0]);
+                            Quants.Add(actualQuant);
+                            actualQuant = 0;
+                        }
+                        verifingDN = actualDn[0];                        
+                    }
+                    dataGridView1.Rows.Add(DNs, Quants);
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"Falha ao ler o arquivo.\n {ex.Message}");
+            }
+        }
+
+      
+    }
+}
